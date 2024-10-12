f02750c54ff7ea82527d0691151c61aaaec052c9
Bharath-M30 <bharathmurugan30@gmail.com>
2024-10-12 09:30:59 +0530

    Card centered

--- Business-Card/src/App.tsx
+++ Business-Card/src/App.tsx
@@ -5,7 +5,7 @@ import Footer from "./components.tsx/Footer"
 
 export default function App(): React.JSX.Element {
     return (
-      <div className="min-h-screen bg-[#23252C] p-4 font-montserrat">
+      <div className="min-h-screen bg-[#23252C] p-4 font-montserrat flex items-center">
         <main className="max-w-[300px] bg-[#1A1B21] mx-auto rounded-3xl">
           <Info />
           <section className="px-6">
